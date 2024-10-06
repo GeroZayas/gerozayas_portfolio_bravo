50a7b3f1ed4e0e742eea244364287e26ca5b4299
Gero Zayas <gerozayas@gmail.com>
2024-10-06 12:03:13 +0200

    New reinstall of decouple

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 # SECRET_KEY = env("SECRET_KEY")
 
-
+# New update
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config("DEBUG", default=False, cast=bool)
